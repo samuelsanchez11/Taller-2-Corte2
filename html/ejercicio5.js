7b47e839f4a7fc23ae511144a2d250ce7150f606
Samuel <samuel.sanchez@cecar.edu.co>
2024-11-02 13:38:31 -0500

    Se realizo el taller

---
+++ html/ejercicio5.js
@@ -0,0 +1,4 @@
+document.getElementById('hideSection').addEventListener('click', function() {
+    const paragraph = document.getElementById('paragraph');
+    paragraph.style.display = 'none';
+});
